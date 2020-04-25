d7dea5f2f74dcdc1af7d2e8761da1fccf89c7282
Rnaaz <rohinanaaz@gmail.com>
2020-04-26 00:43:29 +0530

    Added Property details section and fixed property-schema (#3)

--- ui/assets/js/schema_functions.js
+++ ui/assets/js/schema_functions.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     $("#schema_class_section").hide();
     $("#schema_property_section").hide();
     $('#class_property_section').hide();
+    $('#property_details_section').hide();
     $("#home-section").show();
     // $('#footer').html(getFooterContent())
 });
@@ -20,14 +21,16 @@ function showAllProperties() {
     $("#home-section").hide();
     $("#schema_class_section").hide();
     $('#class_property_section').hide();
+    $('#property_details_section').hide();
     $("#schema_property_section").show();
 }
 
 function showAllClasses() {
     $("#home-section").hide();
     $("#schema_property_section").hide();
-    $("#schema_class_section").show();
     $('#class_property_section').hide();
+    $('#property_details_section').hide();
+    $("#schema_class_section").show();
     $("#footer").show();
 }
 
@@ -35,106 +38,129 @@ function getSchemaClass(__data) {
     return (
         `
     <tr>           
-                        <th class="prop-nam" scope="row">
-      
-                        <code property="rdfs:label"><a href="#` +
-        __data[i]["rdfs:label"] +
-        `" onclick="getClass_property('` +
-        __data[i]["rdfs:label"] +
-        `','` +
-        __data[i]["rdfs:comment"] +
-        `');">` +
-        __data[i]["rdfs:label"] +
-        `</a></code>
-                        </th>
-                        <td>&nbsp;</td>
-                        <td class="prop-desc" property="rdfs:comment">` +
-        __data[i]["rdfs:comment"] +
-        `</td></tr><tr typeof="rdfs:Property" resource="http://schema.org/addressCountry">
-          
-        
-                    </tr>
-                         `
+            <th class="prop-nam" scope="row">
+                              <a href="#` + __data[i]["rdfs:label"] +`" onclick="getClass_property('`+__data[i]["rdfs:label"] +`','` + __data[i]["rdfs:comment"] +`');">`+ __data[i]["rdfs:label"] +`</a>
+            </th>
+            <td>&nbsp;</td><td class="prop-desc">`+ __data[i]["rdfs:comment"] + `</td></tr><tr>
+    </tr">
+    `
     );
 }
 
 function getSchemaProperties(__data) {
     return (
         `
-    <tr>           
-                        <th class="prop-nam" scope="row">
+        <tr>           
+        <th class="prop-nam" scope="row">
+    
+    <a href="#` + __data[i]["rdfs:label"] +`" onclick=" getProperty_details('`+__data[i]["rdfs:label"] +`','` + __data[i]["rdfs:comment"] +`');">`+__data[i]['rdfs:label']+`</a>
+      </th>
+    
+    <td class="prop-desc">`+__data[i]['rdfs:comment']+`</td></tr><tr>
       
-                        <code property="rdfs:label"><a href="#` +
-        __data[i]["rdfs:label"] +
-        `" onclick="getProperty_expanded('` +
-        __data[i]["rdfs:label"] +
-        `','` +
-        __data[i]["rdfs:comment"] +
-        `');">` +
-        __data[i]["rdfs:label"] +
-        `</a></code>
-                        </th>
-                        <td>&nbsp;</td>
-                        <td class="prop-desc" property="rdfs:comment">` +
-        __data[i]["rdfs:comment"] +
-        `</td></tr><tr typeof="rdfs:Property" resource="http://schema.org/addressCountry">
-          
-        
-                    </tr>
-                         `
+    
+    </tr>
+         `
     );
 }
 
 
-function getProperty_expanded(propertyName) {
-    // var schema = "Schema:";
+function getProperty_details(prop_name,prop_desc) {
     $("#schema_class_section").hide();
-    $('#class_property_section').show();
-    $("#class_propertySchema").html("");
+    $('#class_property_section').hide();
+    $("#schema_property_section").hide();
+    $('#property_details_section').show();
+    $('#property_details').html("");
 
-    //  document.location.href = 'https://localhost.iudx.org.in:8443/schema_property';
     $.ajax({
-        url:  "/" + class_label,
+        url: "/" + prop_name,
         type: "GET",
         contentType: "application/json+ld",
         success: function (data) {
             console.log(data["@graph"]);
-            var redirectURL = location.origin + "/" + class_label;
+            var redirectURL = location.origin + "/" + prop_name;
+            var dataResult_graph = data["@graph"];
+                          
+                $("#property_details").append(
+                                `
+                <h1>`+dataResult_graph[0]["rdfs:label"]+`</h1">
+
+                <h4><span class="breadcrumbs">`+(dataResult_graph[0]["@type"][0].split("rdf:"))[1]+` &#47; `+(dataResult_graph[0]["@type"][1]).split("iudx:")[1]+`</span>
+                </h4>
+                
+                <div>`+dataResult_graph[0]["rdfs:comment"]+`</div>
+                
+                <table class="definition-table">
+                <thead>
+                  <tr>
+                    <th>Expected Type Value</th>
+                  </tr>
+                </thead>
+                
+                  <tbody><tr>
+                  `+ getRangeIncludes(dataResult_graph[0]["iudx:rangeIncludes"]) + `
+                    
+                  </tr>
+                </tbody></table>
+                
+                <table class="definition-table">
+                  <thead>
+                    <tr>
+                      <th>Used on these types</th>
+                    </tr>
+                </thead>
+                <tbody>
+                `+ getDomainIncludes(dataResult_graph[0]["iudx:domainIncludes"]) + `
+                </tbody></table> 
+                                      
+                              `
+                            );
+        },
+        error: function (error) { }
+    });
+}
+
+function getClass_property(className, class_desc) {
+    // var schema = "Schema:";
+    $("#schema_class_section").hide();
+    $('#class_property_section').show();
+    $('#schema_class').html("");
+    $("#class_propertySchema").html("");
+
+    //  document.location.href = 'https://localhost.iudx.org.in:8443/schema_property';
+    $.ajax({
+        url: "/" + className,
+        type: "GET",
+        contentType: "application/json+ld",
+        success: function (data) {
+            console.log(data["@graph"]);
+            // var redirectURL = location.origin + "/" + className;
             var dataResult_graph = data["@graph"];
             var arr = [];
             for (i = 0; i < dataResult_graph.length; i++) {
                 if (
                     dataResult_graph[i]["@type"] !== undefined &&
                     dataResult_graph[i]["@type"][0] === "rdf:Property"
-                ) {
-                    // console.log(dataResult_graph[i]["@type"]);
-                    // console.log(dataResult_graph[i]["iudx:rangeIncludes"])
-                    // console.log(dataResult_graph[i]["iudx:rangeIncludes"].length);
-                    // for(k=0; k< dataResult_graph[i]["iudx:rangeIncludes"].length;k++)
-                    // {
-                    //     arr.push(dataResult_graph[i]["iudx:rangeIncludes"][k]['@id'])
-                        
-                    // }
-                    // console.log(arr)
+                ) 
+                    {
+                                           
                     for (j = 0; j < dataResult_graph[i]["iudx:domainIncludes"].length; j++) {
                         // console.log("domainIncludes");data
-                        if (
-                            dataResult_graph[i]["iudx:domainIncludes"][j]["@id"].includes(
-                                "iudx:" + class_label
-                            )
-                        ) {
-                            
-                            $("#class_propertySchema").append(
-                                `
+                        if (dataResult_graph[i]["iudx:domainIncludes"][j]["@id"].includes("iudx:" + className)) {
+
+                            $("#class_label").text(className);
+                            $("#class_desc").text(class_desc);
+                            $("#class_propertySchema").append(`
+                                
                                 <tr>           
-                                    <th class="prop-nam" scope="row"><code property="rdfs:label"><a href="./address">` +
+                                    <th class="prop-nam" scope="row"><a href="#` +dataResult_graph[i]["rdfs:label"] +`" onclick=" getProperty_details('`+dataResult_graph[i]["rdfs:label"] +`','` + dataResult_graph[i]["rdfs:comment"] +`');">` +
                                 dataResult_graph[i]["rdfs:label"] +
-                                `</a></code>
+                                ` </a>
                                     </th>dataResult_graph
-                                   `+getRangeIncludes(dataResult_graph[i]["iudx:rangeIncludes"])+`
-                                    <td class="prop-desc" property="rdfs:comment">` +
+                                   `+ getRangeIncludes(dataResult_graph[i]["iudx:rangeIncludes"]) + `
+                                    <td class="prop-desc">` +
                                 dataResult_graph[i]["rdfs:comment"] +
-                                `</td></tr><tr typeof="rdfs:Property" resource="http://schema.org/addressCountry">
+                                `</td></tr><tr>
                                 </tr>
     
                               `
@@ -149,87 +175,37 @@ function getProperty_expanded(propertyName) {
     });
 }
 
-
-
-function getClass_property(class_label, class_desc) {
-    // var schema = "Schema:";
-    $("#schema_class_section").hide();
-    $('#class_property_section').show();
-    $("#class_propertySchema").html("");
-
-    //  document.location.href = 'https://localhost.iudx.org.in:8443/schema_property';
-    $.ajax({
-        url:  "/" + class_label,
-        type: "GET",
-        contentType: "application/json+ld",
-        success: function (data) {
-            console.log(data["@graph"]);
-            var redirectURL = location.origin + "/" + class_label;
-            var dataResult_graph = data["@graph"];
-            var arr = [];
-            for (i = 0; i < dataResult_graph.length; i++) {
-                if (
-                    dataResult_graph[i]["@type"] !== undefined &&
-                    dataResult_graph[i]["@type"][0] === "rdf:Property"
-                ) {
-                    // console.log(dataResult_graph[i]["@type"]);
-                    // console.log(dataResult_graph[i]["iudx:rangeIncludes"])
-                    // console.log(dataResult_graph[i]["iudx:rangeIncludes"].length);
-                    // for(k=0; k< dataResult_graph[i]["iudx:rangeIncludes"].length;k++)
-                    // {
-                    //     arr.push(dataResult_graph[i]["iudx:rangeIncludes"][k]['@id'])
-                        
-                    // }
-                    // console.log(arr)
-                    for (j = 0; j < dataResult_graph[i]["iudx:domainIncludes"].length; j++) {
-                        // console.log("domainIncludes");data
-                        if (
-                            dataResult_graph[i]["iudx:domainIncludes"][j]["@id"].includes(
-                                "iudx:" + class_label
-                            )
-                        ) {
-                            
-                            $("#class_propertySchema").append(
-                                `
-                                <tr>           
-                                    <th class="prop-nam" scope="row"><code property="rdfs:label"><a href="./address">` +
-                                dataResult_graph[i]["rdfs:label"] +
-                                `</a></code>
-                                    </th>dataResult_graph
-                                   `+getRangeIncludes(dataResult_graph[i]["iudx:rangeIncludes"])+`
-                                    <td class="prop-desc" property="rdfs:comment">` +
-                                dataResult_graph[i]["rdfs:comment"] +
-                                `</td></tr><tr typeof="rdfs:Property" resource="http://schema.org/addressCountry">
-                                </tr>
-    
-                              `
-                            );
-                            // $(location).attr("href", redirectURL);
-                        }
-                    }
-                }
-            }
-        },
-        error: function (error) { }
-    });
-}
-
-function getRangeIncludes(_data){
+function getRangeIncludes(_data) {
     console.log(_data)
     //  for(i=0;i<_data.length;i++){
     //      console.log(_data[i]['@id'])
     //  }
-    if(_data.length === 2){
-        
-    return `
-    <td class="prop-ect"><link property="rangeIncludes" href="http://schema.org/PostalAddress"><a href="./PostalAddress">`+_data[0]['@id']+`</a>&nbsp; or <br> <link property="rangeIncludes" href="http://schema.org/Text"><a href="./Text">`+_data[1]['@id']+`</a>&nbsp;<link property="domainIncludes" href="http://schema.org/GeoCoordinates"><link property="domainIncludes" href="http://schema.org/GeoShape"><link property="domainIncludes" href="http://schema.org/Organization"><link property="domainIncludes" href="http://schema.org/Person"><link property="domainIncludes" href="http://schema.org/Place"></td>
+    if (_data.length === 2) {
+
+        return `
+    <td class="prop-ect">`+ _data[0]['@id'] + `&nbsp; or <br> ` + _data[1]['@id'] + `&nbsp;</td>
 `
 
-}
-else if(_data.length === 1){
-    return `
-    <td class="prop-ect"><link property="rangeIncludes" href="http://schema.org/PostalAddress"><a href="./PostalAddress">`+_data[0]['@id']+`</a></td>
+    }
+    else if (_data.length === 1) {
+        return `
+    <td class="prop-ect">`+ _data[0]['@id'] + `</td>
 `
 
+    }
 }
+
+function getDomainIncludes(_data){
+    console.log(_data)
+    var tableData = [];
+    for(i=0;i< _data.length;i++){
+        tableData[i] =  `
+        <tr>
+        <td>`+_data[i]['@id'].split("iudx:")[1]+`</td> 
+        </tr>
+        `
+    }
+
+    return tableData;
 }
+
