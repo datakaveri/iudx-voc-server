7208b9e047191c0fd0f12ab7887738160b55b6e9
rnaaz <rohinanaaz@gmail.com>
2020-05-13 17:33:18 +0530

    Updated classes & properties

--- ui/src/app/property/property.component.ts
+++ ui/src/app/property/property.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../services/data.service';
+import { Property } from '../types/property';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -10,8 +11,24 @@ import { Observable } from 'rxjs';
 export class PropertyComponent implements OnInit {
   constructor(private backendservice: DataService) {}
 
-  // propertyList: Observable<PropertyResult[]>;
+  propertyList = [];
+  public propertyData: Property[] = [];
   ngOnInit(): void {
-    // this.propertyList = this.backendservice.getAllProperties();
+    this.populatePropertyTable();
+  }
+  populatePropertyTable(): void {
+    this.backendservice.getAllProperties().subscribe(data => {
+      this.propertyList = Array.from(Object.keys(data), k => data[k]);
+      console.log(this.propertyList);
+      for (let pr in this.propertyList) {
+        this.propertyData.push({
+          label: this.propertyList[pr]['rdfs:label'],
+          comment: this.propertyList[pr]['rdfs:comment']
+        });
+      }
+    });
+  }
+  showPropertyDetail(event) {
+    console.log(event);
   }
 }
