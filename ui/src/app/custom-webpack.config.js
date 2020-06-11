e5105cbec74c67b40a64fe025d29b6e49d46234c
rnaaz <rohinanaaz@gmail.com>
2020-06-11 13:29:49 +0530

    Added optimizations for  performance, accessibility, best practices, SEO

--- ui/src/app/custom-webpack.config.js
+++ ui/src/app/custom-webpack.config.js
@@ -1,16 +1,12 @@
-// const CompressionPlugin = require('compression-webpack-plugin');
-
-// module.exports = {
-//     plugins: [new CompressionPlugin()],
-// };
-
+const CompressionPlugin = require('compression-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
-    plugins: [
-        new webpack.DefinePlugin({
-            'STABLE_FEATURE': JSON.stringify(true),
-            'EXPERIMENTAL_FEATURE': JSON.stringify(false)
-        })
-    ]
+    plugins: [new CompressionPlugin(), new webpack.DefinePlugin({
+        'STABLE_FEATURE': JSON.stringify(true),
+        'EXPERIMENTAL_FEATURE': JSON.stringify(false)
+    })]
 };
+
+
+
