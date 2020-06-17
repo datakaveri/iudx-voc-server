67d6e7db01dccad9e42d4a55a9b535b889a86215
rnaaz <rohinanaaz@gmail.com>
2020-06-17 20:40:58 +0530

    Added new page-not-found Component, Updated CSS, implementation for 404 in search-results

---
+++ ui/src/app/page-not-found/page-not-found.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-page-not-found',
+  template: `
+    <h1>
+      page-not-found works!
+    </h1>
+  `,
+  styles: [
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class PageNotFoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
