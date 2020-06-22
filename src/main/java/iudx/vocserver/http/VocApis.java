8c9b41574182733efb5c0273dbc97b244af5aba5
rraks <rakshitadmar@gmail.com>
2020-06-22 10:57:30 +0530

    Updated vocabulary update command to execute twice to be on the safer side

--- src/main/java/iudx/vocserver/http/VocApis.java
+++ src/main/java/iudx/vocserver/http/VocApis.java
@@ -48,7 +48,7 @@ public final class VocApis implements VocApisInterface {
     private static final Logger LOGGER = LoggerFactory.getLogger(HttpServerVerticle.class);
 
     private static String VOC_REPO = "iudx-voc/";
-    private static String UPDATE_REPO_CMD = "nohup git fetch && git reset --hard origin/master &";
+    private static String UPDATE_REPO_CMD = "nohup sleep 5 && git fetch && git reset --hard origin/master &";
     private static String PUSH_SCHEMAS_CMD = "nohup python3 utils/push/hookTriggeredInsert.py &";
 
     /**
@@ -88,6 +88,7 @@ public final class VocApis implements VocApisInterface {
                     .end();
             }
         });
+        Proc.execCommand("cd " + VOC_REPO + " && " + UPDATE_REPO_CMD);
         Proc.execCommand("cd " + VOC_REPO + " && " + UPDATE_REPO_CMD);
         Proc.execCommand("cd " + VOC_REPO + " && " + PUSH_SCHEMAS_CMD);
         context.response()
