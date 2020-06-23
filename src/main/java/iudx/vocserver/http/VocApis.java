d1492679f6dc1d28e9c1bdaad2636c0f371421fd
rraks <rakshitadmar@gmail.com>
2020-06-23 10:06:29 +0530

    Minor modification in api callbacks

--- src/main/java/iudx/vocserver/http/VocApis.java
+++ src/main/java/iudx/vocserver/http/VocApis.java
@@ -89,7 +89,6 @@ public final class VocApis implements VocApisInterface {
             }
         });
         Proc.execCommand("cd " + VOC_REPO + " && " + UPDATE_REPO_CMD);
-        Proc.execCommand("cd " + VOC_REPO + " && " + UPDATE_REPO_CMD);
         Proc.execCommand("cd " + VOC_REPO + " && " + PUSH_SCHEMAS_CMD);
         context.response()
             .putHeader("content-type", "application/json")
