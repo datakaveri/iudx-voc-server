6fecf8b13b343a8376b811c5f7dfca63412eab2f
rnaaz <rohinanaaz@gmail.com>
2020-08-06 17:34:46 +0530

    Implemented JSON Viewer for descriptor details, Modified CSS, installed ngx-json-viewer library

--- ui/src/app/data-descriptors/descriptor-details/descriptor-details.component.ts
+++ ui/src/app/data-descriptors/descriptor-details/descriptor-details.component.ts
@@ -30,23 +30,23 @@ export class DescriptorDetailsComponent implements OnInit {
       console.log(this.value);
     });
     this.details = this.backendService.getDescriptorDetails(this.value);
-    this.details.subscribe((res) => {
-      console.log(typeof res);
-      this.results = res;
-      console.log(this.results);
-      this.resultKeys = Object.keys(res[0]);
-      this.resultValues = Object.values(res[0]);
-      console.log(this.resultKeys);
-      console.log(this.resultValues);
-      this.resultKeys = this.resultKeys.slice(3, 10);
-      this.resultValues = this.resultValues.slice(3, 10);
-      // this.res.push({ key: this.resultKeys, values: this.resultValues });
-      //   var res: any = {
-      //     key: this.resultKeys,
-      //     values: this.resultValues,
-      //   };
-      //   this.results = res;
-      //   console.log(this.results);
-    });
+    // this.details.subscribe((res) => {
+    //   console.log(typeof res);
+    //   this.results = res;
+    //   console.log(this.results);
+    //   this.resultKeys = Object.keys(res[0]);
+    //   this.resultValues = Object.values(res[0]);
+    //   console.log(this.resultKeys);
+    //   console.log(this.resultValues);
+    //   this.resultKeys = this.resultKeys.slice(3, 10);
+    //   this.resultValues = this.resultValues.slice(3, 10);
+    // this.res.push({ key: this.resultKeys, values: this.resultValues });
+    //   var res: any = {
+    //     key: this.resultKeys,
+    //     values: this.resultValues,
+    //   };
+    //   this.results = res;
+    //   console.log(this.results);
+    // });
   }
 }
